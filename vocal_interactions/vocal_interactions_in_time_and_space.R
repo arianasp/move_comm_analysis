d333c4fcdd008ae12bc49ea0da57bb139d3ce683
Ariana Strandburg-Peshkin <arianasp@gmail.com>
2020-12-02 13:11:40 +0100

    fixed self reply vs response analysis - got rid of some bugs

--- vocal_interactions/vocal_interactions_in_time_and_space.R
+++ vocal_interactions/vocal_interactions_in_time_and_space.R
@@ -26,13 +26,13 @@ datadir <- '~/Dropbox/meerkats/meerkats_shared/data'
 codedir <- '~/Dropbox/code_ari/move_comm_analysis/'
 
 #bandwidth of smoothing kernel (default 0.1)
-bw <- .1 
+bw <- .2 
 
 #maximum time lag to consider (time since another individual called)
 max.lag <- 30 
 
 #time step to use for the sequence of times
-step <- .01 
+step <- .05 
 
 #list of call types to include in the set of calls by the initial caller (which determines the 0 point of the correlogram)
 caller.calltypes <- c('cc','cchyb') 
@@ -41,11 +41,11 @@ caller.calltypes <- c('cc','cchyb')
 responder.calltypes <- c('cc','cchyb')
 
 #distance bins to consider for the distance between caller and responder
-dist.bins <- seq(0,12,3)
+dist.bins <- seq(0,15,5)
 
 #whether to use self-responses instead of responses to others, for an analysis of individual calling periodicity (defaults to FALSE)
 #normally this should be set to FALSE
-self.responses <- FALSE 
+self.responses <- FALSE
 
 #whether to use nonfocal calls within the same file as the 'caller' with the individual whose reocrding it is as 'respodner'
 #this is mainly to check that the result is not driven by some weird synching issues, because we use the data frome the same recording
@@ -53,12 +53,11 @@ self.responses <- FALSE
 use.nonfoc.as.triggers <- FALSE 
 
 #whether to instead perform an analysis of whether an individual repeats its call in a certain time window after its initial call
-#as a function of whether there have been any calls from nearby individuals (within repeat.dist.thresh.post meters, default 30) in the intervening time
+#as a function of whether there have been any calls from nearby individuals (within repeat.dist.thresh meters) in the intervening time
 #all sequences after a call are removed if there has been a call by anyone in the past repeat.time.thresh seconds
 repeat.self.analysis <- TRUE
-repeat.dist.thresh.prev <- 5
-repeat.dist.thresh.post <- 5
 repeat.time.thresh <- 10
+repeat.dist.thresh <- 10
 
 #---------------------SETUP-----------------------
 
@@ -119,8 +118,8 @@ for(d in 1:length(dates)){
   calls.date <- calls.all[which(calls.all$date == date),]
   
   #get start and stop times for times when all are labeled - need to fix this a bit because sometimes there are multiiple start markers (e.g. VHMF010 on 20190712)
-  starts <- calls.date[which(calls.date$callType %in% c('start')),c('ind','t0')]
-  ends <- calls.date[which(calls.date$callType %in% c('stop','end')),c('ind','t0')]
+  starts <- calls.date[which(calls.date$callType %in% c('start','START')),c('ind','t0')]
+  ends <- calls.date[which(calls.date$callType %in% c('stop','end','STOP','END')),c('ind','t0')]
   
   #for now just take the minimum 'start' for each individual as its start marker and the max end marker as its end time
   starts <- aggregate(starts$t0,by = list(starts$ind), min)
@@ -209,7 +208,7 @@ for(i in 1:nrow(callresp.seqs)){
   zero.time <- callresp$t0[i]
   
   #if too close to end, don't use
-  if((zero.time - max.lag > callresp$start[i]) & (zero.time + max.lag < callresp$end[i])){
+  if(((zero.time - max.lag) > callresp$start[i]) & ((zero.time + max.lag) < callresp$end[i])){
     
     #get call times of the responder (only of the correct call type)
     call.times.foc <- ccs.all$t0[which(ccs.all$caller==callresp$responder[i] & (ccs.all$callSimple %in% responder.calltypes))]
@@ -241,127 +240,75 @@ for(i in 1:nrow(callresp.seqs)){
 if(repeat.self.analysis == T){
   
   #----SETUP---
-  #first the time of the previous call (by anyone within repeat.dist.thresh.prev of the caller), the caller id, and the call type
+  #first the time of the previous call (by anyone within repeat.dist.thresh of the caller), the caller id, and the call type
   callresp$prevcall.t0 <- NA
   callresp$prevcall.caller <- NA
   callresp$prevcall.type <- NA
-  callresp$n.inds.in.range.prev <- NA
+  callresp$n.inds.in.range <- NA
+  
+  #time of the next call (by anyone within repeat.dist.thresh of the caller), the caller id, and the call type
+  callresp$nextcall.t0 <- NA
+  callresp$nextcall.caller <- NA
+  callresp$nextcall.type <- NA
+  
   for(i in 1:nrow(callresp)){
-    print(i)
     
     #get time of the focal call
     t0 <- callresp$t0[i] 
     
     #time index in timeLine
     t0.round <- round(t0)
-    t.idx <- which(ts == t0.round)
+    t.idx <- which(timeline.numeric == t0.round)
+    
+    #get individual index
+    ind.idx <- which(ind.info$code == callresp$caller[i])
     
     #distances to all other individuals present
-    dists <- sqrt((allX[-c(ind.idx),t.idx] - allX[ind.idx,t.idx])^2 + (allY[-c(ind.idx),t.idx] - allY[ind.idx,t.idx])^2)
+    dists <- sqrt((allX[,t.idx] - allX[ind.idx,t.idx])^2 + (allY[,t.idx] - allY[ind.idx,t.idx])^2)
     
-    #get the individuals that are in range (wihtin repeat.dist.thresh.prev)
-    inds.in.range.idxs <- which(dists < repeat.dist.thresh.prev)
+    #distance to self is NA
+    dists[ind.idx] <- NA
     
-    #store number of individuals in range
-    callresp$n.inds.in.range.prev[i] <- length(inds.in.range.idxs)
-    
-    #if there are some indivudals in range, find the most recent call from any of them of any type
-    if(length(inds.in.range.idxs)>0){
-      inds.in.range <- indInfo$code[inds.in.range.idxs]
-      calls.inds.in.range <- calls.all[which(calls.all$ind %in% inds.in.range),]
-      prev.call.time.inds.in.range <- max(calls.inds.in.range$t0[which(calls.inds.in.range$t0 < t0)])
-      callresp$prevcall.t0[i] <- as.numeric(prev.call.time.inds.in.range)
-      prev.call.inds.in.range.idx <- which(calls.inds.in.range$t0 == prev.call.time.inds.in.range)[1]
-      callresp$prevcall.caller[i] <- calls.inds.in.range$ind[prev.call.inds.in.range.idx]
-      callresp$prevcall.type[i] <- calls.inds.in.range$callSimple[prev.call.inds.in.range.idx]
-    }
-    
-    #get all calls prior to that
-    #calls.prior <- which(calls.all$isCall==1 & ((calls.all$t0 - t0) < 0))
-    
-    #find the most recent call
-    #most.recent.call.time <- max(calls.all$t0[calls.prior])
-    
-    #if(!is.na(most.recent.call.time)){
-    #  most.recent.call.row <- which(calls.all$t0 == most.recent.call.time)[1]
-    
-      #store information on that call in the data frame
-    #  callresp$prevcall.t0[i] <- as.numeric(calls.all$t0[most.recent.call.row])
-    #  callresp$prevcall.caller[i] <- calls.all$ind[most.recent.call.row]
-    #  callresp$prevcall.type[i] <- calls.all$callSimple[most.recent.call.row]
-    #}
-  }
-  
-  #find the time of the next non-focal call from an individual within repeat.dist.thresh.post
-  ts <- as.numeric(timeLine)
-  callresp$n.inds.in.range <- NA
-  callresp$nextcall.t0 <- NA
-  callresp$nextcall.caller <- NA
-  callresp$nextcall.type <- NA
-  for(i in 1:nrow(callresp)){
-    
-    #time of call
-    t0 <- callresp$t0[i]
-    
-    #caller
-    ind <- callresp$caller[i]
-    ind.idx <- which(indInfo$code == ind)
-    
-    #time index in timeLine
-    t0.round <- round(t0)
-    t.idx <- which(ts == t0.round)
-    
-    #distances to all other individuals present
-    dists <- sqrt((allX[-c(ind.idx),t.idx] - allX[ind.idx,t.idx])^2 + (allY[-c(ind.idx),t.idx] - allY[ind.idx,t.idx])^2)
-    
-    #get the individuals that are in range (wihtin repeat.dist.thresh.post)
-    inds.in.range.idxs <- which(dists < repeat.dist.thresh.post)
+    #get the individuals that are in range (wihtin repeat.dist.thresh)
+    inds.in.range.idxs <- which(dists < repeat.dist.thresh)
     
     #store number of individuals in range
     callresp$n.inds.in.range[i] <- length(inds.in.range.idxs)
     
-    #if there are some indivudals in range, find the next call from any of them of a type in responder.calltypes
+    #if there are some indivudals in range, find the most recent call from any of them of any type, and next call of a type in responder.calltypes  
     if(length(inds.in.range.idxs)>0){
+      
+      #get individuals in range at the time of the focal call
       inds.in.range <- indInfo$code[inds.in.range.idxs]
-      calls.inds.in.range <- calls.all[which(calls.all$ind %in% inds.in.range),]
-      next.call.time.inds.in.range <- min(calls.inds.in.range$t0[which(calls.inds.in.range$t0 > t0 & calls.inds.in.range$callSimple %in% responder.calltypes)])
-      callresp$nextcall.t0[i] <- as.numeric(next.call.time.inds.in.range)
-      next.call.inds.in.range.idx <- which(calls.inds.in.range$t0 == next.call.time.inds.in.range)[1]
-      callresp$nextcall.caller[i] <- calls.inds.in.range$ind[next.call.inds.in.range.idx]
-      callresp$nextcall.type[i] <- calls.inds.in.range$callSimple[next.call.inds.in.range.idx]
+      
+      #get all calls from the individuals in range
+      calls.inds.in.range <- calls.all[which(calls.all$ind %in% inds.in.range & calls.all$isCall==1 & calls.all$unsureFocal==0 & calls.all$nonFocal==0),]
+      
+      #get most recent previous call from the individuals in range (of any type)
+      calls.before <- calls.inds.in.range$t0[which(calls.inds.in.range$t0 < t0)]
+      if(length(calls.before) > 0){
+        prev.call.time.inds.in.range <- max(calls.before)
+        callresp$prevcall.t0[i] <- as.numeric(prev.call.time.inds.in.range, tz = 'UTC')
+        prev.call.inds.in.range.idx <- which(calls.inds.in.range$t0 == prev.call.time.inds.in.range)[1]
+        callresp$prevcall.caller[i] <- calls.inds.in.range$ind[prev.call.inds.in.range.idx]
+        callresp$prevcall.type[i] <- calls.inds.in.range$callSimple[prev.call.inds.in.range.idx]
+      }
+      
+      #get next call from individuals in range (of types in responder.calltypes)
+      correct.calls.after <- calls.inds.in.range$t0[which(calls.inds.in.range$t0 > t0 & calls.inds.in.range$callSimple %in% responder.calltypes)]
+      if(length(correct.calls.after)>0){
+        correct.calls.after.numeric <- as.numeric(correct.calls.after, tz = 'UTC')
+        next.call.time.inds.in.range <- min(correct.calls.after.numeric)
+        callresp$nextcall.t0[i] <- as.numeric(next.call.time.inds.in.range)
+        next.call.inds.in.range.idx <- which(calls.inds.in.range$t0 == next.call.time.inds.in.range)[1]
+        callresp$nextcall.caller[i] <- calls.inds.in.range$ind[next.call.inds.in.range.idx]
+        callresp$nextcall.type[i] <- calls.inds.in.range$callSimple[next.call.inds.in.range.idx]
+      }
     }
   }
+  
   callresp$dt.prev <- callresp$t0 - callresp$prevcall.t0
   callresp$dt.next <- callresp$nextcall.t0 - callresp$t0
-  
-  #---ANALYSIS---
-  
-  #set up vectors to hold output for call rather at a time point after a call when there IS vs IS NOT a response
-  callrate.noresp <- callrate.resp <- rep(NA, length(bins)-1)
-  
-  ##bins to measure individual call rate over (after a focal call), get response sequences only after t0 = 0
-  bins <- tseq[which(tseq >= 0)]
-  callresp.seqs.repeat <- callresp.seqs[,which(tseq >= 0)]
-  
-  #loop over bins to compute call rates when there has or hasn't been a response
-  callrate.resp <- callrate.nonresp <- n.resp <- n.nonresp <- rep(NA, length(bins)-1)
-  for(i in 2:length(bins)){
-    response.idxs <- which(callresp$dt.next < bins[i] & callresp$dt.prev > repeat.time.thresh)
-    nonresponse.idxs <- which(callresp$dt.next > bins[i] & callresp$dt.prev > repeat.time.thresh)
-    
-    callrate.resp[i] <- mean(callresp.seqs.repeat[response.idxs,i],na.rm=T)
-    callrate.nonresp[i] <- mean(callresp.seqs.repeat[nonresponse.idxs,i],na.rm=T)
-    n.resp[i] <- length(response.idxs)
-    n.nonresp[i] <- length(nonresponse.idxs)
-  }
-  
-  quartz(height = 12, width = 8)
-  par(mfrow=c(2,1))
-  plot(bins, callrate.resp, type='l', col = 'red', lwd = 2, xlab = 'Time after call (sec)', ylab = c('Call rate'), cex.lab = 1.5, main = paste('Min silence = ', repeat.time.thresh, ' | Max dist prev = ', repeat.dist.thresh.prev, ' | Max dist post = ', repeat.dist.thresh.post))
-  lines(bins, callrate.nonresp, col = 'blue', lwd = 2, lty = 1)
-  legend('topright', legend = c('reply', 'no reply'), col = c('red','blue'), lty = c(1,1), cex = 1.5)
-  plot(bins, n.resp, type = 'l', lwd = 2, xlab = 'Time (sec)', ylab = 'Number of events', col = 'red', cex = 2, cex.lab = 1.5, ylim = c(0, max(n.resp + n.nonresp,na.rm=T)))
-  lines(bins,n.nonresp, lwd = 2, col = 'blue')
 }
 
 #---------------------PLOTTING-------------------
@@ -423,4 +370,38 @@ for(i in 1:length(inds)){
   plot(tseq, mean.call.rates[i,], col = cols[i], lwd = 2,type='l',xlim=c(-5,5),ylim=c(0,ymax),xlab='Time lag (sec)', ylab = 'Call rate', cex.axis=1.5,cex.lab=1.5)
   abline(v=0, lty=2)
   legend(x = 'topleft',legend=inds[i],col=cols[i],lwd=1.5, cex=1.5)
+}
+
+#if performing analysis of whether calls are repeated when a reply is vs isn't given, make this plot
+if(repeat.self.analysis){
+  ##bins to measure individual call rate over (after a focal call), get response sequences only after t0 = 0
+  bins <- tseq[which(tseq >= 0)]
+  
+  #get only positive time lags
+  callresp.seqs.repeat <- callresp.seqs[,which(tseq >= 0)]
+  
+  #loop over bins to compute call rates when there has or hasn't been a response
+  callrate.resp <- callrate.nonresp <- callrate.nonbrs <- n.resp <- n.nonresp <- n.nonbr <- rep(NA, length(bins)-1)
+  for(i in 2:length(bins)){
+    response.idxs <- which(callresp$dt.next < bins[i] & callresp$dt.prev > repeat.time.thresh)
+    nonresponse.idxs <- which(callresp$dt.next > bins[i] & callresp$dt.prev > repeat.time.thresh)
+    noneighbor.idxs <- which(callresp$n.inds.in.range == 0)
+    
+    callrate.resp[i] <- mean(callresp.seqs.repeat[response.idxs,i],na.rm=T)
+    callrate.nonresp[i] <- mean(callresp.seqs.repeat[nonresponse.idxs,i],na.rm=T)
+    callrate.nonbrs[i] <- mean(callresp.seqs.repeat[noneighbor.idxs,i],na.rm=T)
+    n.resp[i] <- length(response.idxs)
+    n.nonresp[i] <- length(nonresponse.idxs)
+    n.nonbr[i] <- length(noneighbor.idxs)
+  }
+  
+  quartz(height = 12, width = 8)
+  par(mfrow=c(2,1))
+  plot(bins, callrate.resp, type='l', col = 'red', lwd = 2, xlab = 'Time after call (sec)', ylab = c('Call rate'), cex.lab = 1.5, main = paste('Min silence = ', repeat.time.thresh, ' | Max dist = ', repeat.dist.thresh), ylim = c(0,0.2))
+  lines(bins, callrate.nonresp, col = 'blue', lwd = 2, lty = 1)
+  lines(bins, callrate.nonbrs, col = 'black', lwd = 2, lty = 1)
+  legend('topright', legend = c('reply', 'no reply','no neighbor'), col = c('red','blue','black'), lty = c(1,1), cex = 1.5)
+  plot(bins, n.resp, type = 'l', lwd = 2, xlab = 'Time (sec)', ylab = 'Number of events', col = 'red', cex = 2, cex.lab = 1.5, ylim = c(0, max(n.resp + n.nonresp + n.nonbr,na.rm=T)))
+  lines(bins,n.nonresp, lwd = 2, col = 'blue')
+  lines(bins,n.nonbr, lwd = 2, col = 'black')
 }
